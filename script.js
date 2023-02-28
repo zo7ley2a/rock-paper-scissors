bf1a2f96b1b4622e67a9750c8c650d2b19ffef5a
Abdelrahman Elsayed <aaelsayyed@gmail.com>
2023-02-28 17:41:47 +0200

    Implement playRound function

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+function getComputerChoice() {
+  let choices = ["rock", "paper", "scissors"];
+  let computerChoice = choices[Math.floor(Math.random() * (3 - 0) + 0)];
+  return computerChoice;
+}
 
-function getComputerChoice(){
-    let choices = ['rock', 'paper', 'scissors'];
-    let computerChoice = choices[Math.floor(Math.random() * (3 - 0) + 0)];
-    return computerChoice;
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "It's a Tie!";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return `You Won! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return `You Lost! ${computerSelection} beats ${playerSelection}`;
+  }
 }
