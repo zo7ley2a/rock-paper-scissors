35201a91a08a7a9e122b84c43e43a3db0c6de60b
Abdelrahman Elsayed <aaelsayyed@gmail.com>
2023-02-28 19:56:34 +0200

    Implement game function

--- script.js
+++ script.js
@@ -6,20 +6,43 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
   let message = "";
-  let result = [];
+  let resultOfRound = [];
   if (playerSelection === computerSelection) {
     message = "It's a Tie!";
-    result = [message, 0, 0];
+    resultOfRound = [message, 0, 0];
   } else if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")
   ) {
     message = `You Won! ${playerSelection} beats ${computerSelection}`;
-    result = [message, 1, 0];
+    resultOfRound = [message, 1, 0];
   } else {
     message = `You Lost! ${computerSelection} beats ${playerSelection}`;
-    result = [message, 0, 1];
+    resultOfRound = [message, 0, 1];
   }
-  return result;
+  return resultOfRound;
 }
+
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("Enter your weapon!").toLowerCase();
+    let computerSelection = getComputerChoice();
+    let currentRound = playRound(playerSelection, computerSelection);
+    playerScore += currentRound[1];
+    computerScore += currentRound[2];
+    console.log(currentRound[0]);
+  }
+  if (playerScore > computerScore) {
+    console.log("You Won The Game!");
+  } else if (computerScore > playerScore) {
+    console.log("You Lost The Game!");
+  } else {
+    console.log("No One Won This Game!");
+  }
+}
+
+game();
